22bc2a67f78019c8260024e1c622b8af90b9d609
leleact <leleact@gmail.com>
2015-05-28 20:14:13 +0800

    basic_string

--- basic/basic_string.h
+++ basic/basic_string.h
@@ -5,7 +5,12 @@ namespace vl
 #define _BASIC_STRING_H
 template <typename T>
 class basic_string {
+  public:
+    basic_string();
+    basic_string(const T *);
 
+  private:
+    T *handle;
 };
 #endif
 }
