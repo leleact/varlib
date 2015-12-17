87e21ffa6248af7d0f2240025eeb35292191e100
lelealien <leleact@gmail.com>
2015-12-17 22:12:22 +0800

    varlib

--- include/varlib.h
+++ include/varlib.h
@@ -1,7 +1,11 @@
 
-#ifndef _VARLIB_BEGIN_NAMESPACE
+#ifndef _VARLIB_H
+#define _VARLIB_H
+
+#define BEGIN_NAMESPACE(x) namespace x {
+#define END_NAMESPACE }
+
 #define _VARLIB_BEGIN_NAMESPACE(x) namespace x {
-#endif
-#ifndef _VARLIB_END_NAMESPACE
 #define _VARLIB_END_NAMESPACE	}
+
 #endif
