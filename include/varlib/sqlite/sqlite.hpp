89e06140b00eade1c565b76908f62b1c58a450e4
leleact <leleact@gmail.com>
2019-10-24 17:03:28 +0800

    feat: sqlite3 c++ interface
    sqlite3 c++ interface

--- include/varlib/sqlite/sqlite.hpp
+++ include/varlib/sqlite/sqlite.hpp
@@ -4,9 +4,11 @@
 #define _SQLITE_H
 
 #include <cstring>
-#include <exception>
+#include <iostream>
 #include <memory>
 #include <sqlite3.h>
+#include <sstream>
+#include <stdexcept>
 #include <utility>
 #include <varlib/varlib.h>
 
@@ -14,18 +16,29 @@ VARLIB_NAMESPACE_BEGIN
 
 namespace sqlite_v3 {
 
-enum init_state { OK = 0L, ERR = 1L << 0 };
+class sqlite_exception : public std::runtime_error {
+public:
+  sqlite_exception(const int &__rc, const std::string &__errMsg)
+      : runtime_error(__errMsg.c_str()), _M_sqlite_rc(__rc),
+        _M_sqlite_msg(__errMsg){};
 
-class sqlite_exception : public std::exception {};
+private:
+  int _M_sqlite_rc;
+  std::string _M_sqlite_msg;
+};
+
+class session;
+
+struct util {
+  static inline void check(const session &__s, const int &__rc);
+};
 
 class session {
   friend class stmt;
 
 public:
-  session() : _M_db_location(""), _M_conn(nullptr), state(ERR) {}
-
   session(const std::string &__location)
-      : _M_db_location(__location), _M_conn(nullptr), state(ERR) {
+      : _M_db_location(__location), _M_conn(nullptr) {
     _M_open_db();
   }
 
@@ -35,8 +48,7 @@ public:
 
   session(session &&__session)
       : _M_db_location(std::move(__session._M_db_location)),
-        _M_conn(std::exchange(__session._M_conn, nullptr)),
-        state(std::exchange(__session.state, ERR)) {}
+        _M_conn(std::exchange(__session._M_conn, nullptr)) {}
 
   session &operator=(const session &__session) {
     _M_close_db();
@@ -49,59 +61,64 @@ public:
     _M_close_db();
     _M_db_location = std::move(__session._M_db_location);
     _M_conn = std::exchange(__session._M_conn, nullptr);
-    state = std::exchange(__session.state, ERR);
     return *this;
   }
 
   ~session() { _M_close_db(); }
 
-  bool is_initialied() const { return state == OK; }
+  const int exec(const char *__str) const {
+    const int &__rc = sqlite3_exec(_M_conn, __str, nullptr, nullptr, nullptr);
+    util::check(*this, __rc);
+    return sqlite3_changes(_M_conn);
+  }
 
-protected:
-  init_state state;
+  const int changes() const { return sqlite3_changes(_M_conn); }
+
+  const int err_code() const { return sqlite3_errcode(_M_conn); }
+
+  const char *err_msg() const { return sqlite3_errmsg(_M_conn); }
 
 private:
   std::string _M_db_location;
   sqlite3 *_M_conn;
 
   inline void _M_open_db() {
-    int rc = sqlite3_open(_M_db_location.c_str(), &_M_conn);
-    if (rc == SQLITE_OK) {
-      state = OK;
-    } else {
-      state = ERR;
+    int __rc = sqlite3_open(_M_db_location.c_str(), &_M_conn);
+    if (__rc != SQLITE_OK) {
+      throw sqlite_exception{__rc, nullptr};
     }
   }
 
   inline void _M_close_db() { sqlite3_close(_M_conn); }
 };
 
+template <typename T> struct ret_type { typedef T type; };
+
 class stmt {
 
 public:
+  enum { ROW, DONE };
+
   stmt(const session &__session, const std::string &__stmt)
-      : _M_session(__session), _M_stmt(__stmt), _M_sqlite_stmt(nullptr),
-        _M_state(ERR) {
+      : _M_session(__session), _M_stmt(__stmt), _M_sqlite_stmt(nullptr) {
     _M_init_stmt();
   }
 
   stmt(const stmt &__stmt)
       : _M_session(__stmt._M_session), _M_stmt(__stmt._M_stmt),
-        _M_sqlite_stmt(nullptr), _M_state(ERR) {
+        _M_sqlite_stmt(nullptr) {
     _M_init_stmt();
   }
 
   stmt(stmt &&__stmt)
       : _M_session(std::move(__stmt._M_session)),
         _M_stmt(std::move(__stmt._M_stmt)),
-        _M_sqlite_stmt(std::exchange(__stmt._M_sqlite_stmt, nullptr)),
-        _M_state(std::exchange(__stmt._M_state, ERR)) {}
+        _M_sqlite_stmt(std::exchange(__stmt._M_sqlite_stmt, nullptr)) {}
 
   stmt &operator=(const stmt &__stmt) {
     _M_session = __stmt._M_session;
     _M_stmt = __stmt._M_stmt;
     _M_sqlite_stmt = nullptr;
-    _M_state = ERR;
     _M_init_stmt();
     return *this;
   }
@@ -111,94 +128,284 @@ public:
     _M_session = std::move(__stmt._M_session);
     _M_stmt = std::move(__stmt._M_stmt);
     _M_sqlite_stmt = std::exchange(__stmt._M_sqlite_stmt, nullptr);
-    _M_state = std::exchange(__stmt._M_state, ERR);
     return *this;
   }
-
   ~stmt() { _M_finalize_stmt(); }
 
-  template <typename T>
-  void bind(const int, T &&,
-            const sqlite3_destructor_type & = SQLITE_TRANSIENT) {}
+  template <typename I, typename T>
+  inline void bind(const I &, const T &,
+                   const sqlite3_destructor_type & = SQLITE_TRANSIENT);
 
-  template <>
-  void bind(const int __index, const std::string &__str,
-            const sqlite3_destructor_type &__sqlite_destructor) {
-    const int rc = sqlite3_bind_text(_M_sqlite_stmt, __index, __str.c_str(), -1,
-                                     __sqlite_destructor);
-    check(rc);
-  }
+  template <typename I, typename T>
+  inline void bind(const I *, const T &,
+                   const sqlite3_destructor_type & = SQLITE_TRANSIENT);
 
-  template <>
-  void bind(const int __index, const char *&__str,
-            const sqlite3_destructor_type &__sqlite_destructor) {
-    int rc = sqlite3_bind_text(_M_sqlite_stmt, __index, __str, -1,
-                               __sqlite_destructor);
-    check(rc);
-  }
+  template <typename I, typename T>
+  inline void bind(const I &, const T *,
+                   const sqlite3_destructor_type & = SQLITE_TRANSIENT);
 
-  template <>
-  void bind(const int __index, const int &__value,
-            const sqlite3_destructor_type &__sqlite_destructor) {
-    int rc = sqlite3_bind_int(_M_sqlite_stmt, __index, __value);
-    check(rc);
-  }
+  template <typename I, typename T>
+  inline void bind(const I *, const T *,
+                   const sqlite3_destructor_type & = SQLITE_TRANSIENT);
 
-  template <>
-  void bind(const int __index, const unsigned &__value,
-            const sqlite3_destructor_type &__sqlite_destructor) {
-    int rc = sqlite3_bind_int64(_M_sqlite_stmt, __index, __value);
-    check(rc);
-  }
+  template <typename I, typename T>
+  inline void bind(const I &, const T *, const int &,
+                   const sqlite3_destructor_type & = SQLITE_TRANSIENT);
 
-  template <>
-  void bind(const int __index, const long &__value,
-            const sqlite3_destructor_type &__sqlite_destructor) {
-    int rc = sqlite3_bind_int64(_M_sqlite_stmt, __index, __value);
-    check(rc);
-  }
-
-  template <>
-  void bind(const int __index, const double &__value,
-            const sqlite3_destructor_type &__sqlite_destructor) {
-    int rc = sqlite3_bind_double(_M_sqlite_stmt, __index, __value);
-    check(rc);
-  }
+  template <typename I, typename T>
+  inline void bind(const I *, const T *, const int &,
+                   const sqlite3_destructor_type & = SQLITE_TRANSIENT);
 
   template <typename T>
-  void bind(const int, T &&, const int,
-            const sqlite3_destructor_type & = SQLITE_TRANSIENT) {}
+  inline const typename ret_type<T>::type get(const int &);
 
-  template <>
-  void bind(const int __index, const void *&__value, const int __size,
-            const sqlite3_destructor_type &__sqlite_destructor) {
-    int rc = sqlite3_bind_blob(_M_sqlite_stmt, __index, __value, __size,
-                               __sqlite_destructor);
-    check(rc);
+  inline const int get_col_count() {
+    return sqlite3_column_count(_M_sqlite_stmt);
+  }
+
+  inline const int exec() {
+    const int rc = sqlite3_step(_M_sqlite_stmt);
+    if (rc == SQLITE_ROW) {
+      return ROW;
+    } else if (SQLITE_DONE == rc) {
+      return DONE;
+    } else {
+      _M_check(rc);
+    }
+    return rc;
+  }
+
+  inline void reset() const {
+    const int __rc = sqlite3_reset(_M_sqlite_stmt);
+    _M_check(__rc);
+  }
+
+  inline void clear_bind() const {
+    const int __rc = sqlite3_clear_bindings(_M_sqlite_stmt);
+    _M_check(__rc);
   }
 
 private:
   session _M_session;
   std::string _M_stmt;
   sqlite3_stmt *_M_sqlite_stmt;
-  init_state _M_state;
 
   inline void _M_init_stmt() {
-    int rc = sqlite3_prepare_v2(_M_session._M_conn, _M_stmt.c_str(),
-                                _M_stmt.size(), &_M_sqlite_stmt, NULL);
-    if (rc == SQLITE_OK) {
-      _M_state = OK;
-    } else {
-      _M_state = ERR;
+    int __rc = sqlite3_prepare_v2(_M_session._M_conn, _M_stmt.c_str(),
+                                  _M_stmt.size(), &_M_sqlite_stmt, NULL);
+    if (__rc != SQLITE_OK) {
+      throw sqlite_exception{__rc, _M_session.err_msg()};
     }
   }
 
   inline void _M_finalize_stmt() { sqlite3_finalize(_M_sqlite_stmt); }
 
-  inline void check(const int __rc) {
-    // TODO check response rc
+  inline void _M_check(const int &__rc) const {
+    if (SQLITE_OK != __rc) {
+      throw sqlite_exception{__rc, _M_session.err_msg()};
+    }
   }
 };
+
+//{@ bind
+template <>
+inline void stmt::bind(const int &__index, const std::string &__value,
+                       const sqlite3_destructor_type &__sqlite_destructor) {
+  const int rc = sqlite3_bind_text(_M_sqlite_stmt, __index, __value.c_str(), -1,
+                                   __sqlite_destructor);
+  _M_check(rc);
+}
+
+template <>
+inline void stmt::bind(const int &__index, const char *__value,
+                       const sqlite3_destructor_type &__sqlite_destructor) {
+  const int rc = sqlite3_bind_text(_M_sqlite_stmt, __index, __value, -1,
+                                   __sqlite_destructor);
+  _M_check(rc);
+}
+
+template <>
+inline void stmt::bind(const int &__index, const int &__value,
+                       const sqlite3_destructor_type &__sqlite_destructor) {
+  int rc = sqlite3_bind_int(_M_sqlite_stmt, __index, __value);
+  _M_check(rc);
+}
+
+template <>
+inline void stmt::bind(const int &__index, const unsigned &__value,
+                       const sqlite3_destructor_type &__sqlite_destructor) {
+  int rc = sqlite3_bind_int64(_M_sqlite_stmt, __index, __value);
+  _M_check(rc);
+}
+
+template <>
+inline void stmt::bind(const int &__index, const long &__value,
+                       const sqlite3_destructor_type &__sqlite_destructor) {
+  int rc = sqlite3_bind_int64(_M_sqlite_stmt, __index, __value);
+  _M_check(rc);
+}
+
+template <>
+inline void stmt::bind(const int &__index, const double &__value,
+                       const sqlite3_destructor_type &__sqlite_destructor) {
+  int rc = sqlite3_bind_double(_M_sqlite_stmt, __index, __value);
+  _M_check(rc);
+}
+
+template <>
+inline void stmt::bind(const std::string &__name, const std::string &__value,
+                       const sqlite3_destructor_type &__sqlite_destructor) {
+  const int __index =
+      sqlite3_bind_parameter_index(_M_sqlite_stmt, __name.c_str());
+  const int rc = sqlite3_bind_text(_M_sqlite_stmt, __index, __value.c_str(), -1,
+                                   __sqlite_destructor);
+  _M_check(rc);
+}
+
+template <>
+inline void stmt::bind(const std::string &__name, const char *__value,
+                       const sqlite3_destructor_type &__sqlite_destructor) {
+  const int __index =
+      sqlite3_bind_parameter_index(_M_sqlite_stmt, __name.c_str());
+  const int rc = sqlite3_bind_text(_M_sqlite_stmt, __index, __value, -1,
+                                   __sqlite_destructor);
+  _M_check(rc);
+}
+
+template <>
+inline void stmt::bind(const std::string &__name, const int &__value,
+                       const sqlite3_destructor_type &__sqlite_destructor) {
+  const int __index =
+      sqlite3_bind_parameter_index(_M_sqlite_stmt, __name.c_str());
+  int rc = sqlite3_bind_int(_M_sqlite_stmt, __index, __value);
+  _M_check(rc);
+}
+
+template <>
+inline void stmt::bind(const std::string &__name, const unsigned &__value,
+                       const sqlite3_destructor_type &__sqlite_destructor) {
+  const int __index =
+      sqlite3_bind_parameter_index(_M_sqlite_stmt, __name.c_str());
+  int rc = sqlite3_bind_int64(_M_sqlite_stmt, __index, __value);
+  _M_check(rc);
+}
+
+template <>
+inline void stmt::bind(const std::string &__name, const long &__value,
+                       const sqlite3_destructor_type &__sqlite_destructor) {
+  const int __index =
+      sqlite3_bind_parameter_index(_M_sqlite_stmt, __name.c_str());
+  int rc = sqlite3_bind_int64(_M_sqlite_stmt, __index, __value);
+  _M_check(rc);
+}
+
+template <>
+inline void stmt::bind(const std::string &__name, const double &__value,
+                       const sqlite3_destructor_type &__sqlite_destructor) {
+  const int __index =
+      sqlite3_bind_parameter_index(_M_sqlite_stmt, __name.c_str());
+  int rc = sqlite3_bind_double(_M_sqlite_stmt, __index, __value);
+  _M_check(rc);
+}
+
+template <>
+inline void stmt::bind(const char *__name, const std::string &__value,
+                       const sqlite3_destructor_type &__sqlite_destructor) {
+  const int __index = sqlite3_bind_parameter_index(_M_sqlite_stmt, __name);
+  const int rc = sqlite3_bind_text(_M_sqlite_stmt, __index, __value.c_str(), -1,
+                                   __sqlite_destructor);
+  _M_check(rc);
+}
+
+template <>
+inline void stmt::bind(const char *__name, const char *__value,
+                       const sqlite3_destructor_type &__sqlite_destructor) {
+  const int __index = sqlite3_bind_parameter_index(_M_sqlite_stmt, __name);
+  const int rc = sqlite3_bind_text(_M_sqlite_stmt, __index, __value, -1,
+                                   __sqlite_destructor);
+  _M_check(rc);
+}
+
+template <>
+inline void stmt::bind(const char *__name, const int &__value,
+                       const sqlite3_destructor_type &__sqlite_destructor) {
+  const int __index = sqlite3_bind_parameter_index(_M_sqlite_stmt, __name);
+  int rc = sqlite3_bind_int(_M_sqlite_stmt, __index, __value);
+  _M_check(rc);
+}
+
+template <>
+inline void stmt::bind(const char *__name, const unsigned &__value,
+                       const sqlite3_destructor_type &__sqlite_destructor) {
+  const int __index = sqlite3_bind_parameter_index(_M_sqlite_stmt, __name);
+  int rc = sqlite3_bind_int64(_M_sqlite_stmt, __index, __value);
+  _M_check(rc);
+}
+
+template <>
+inline void stmt::bind(const char *__name, const long &__value,
+                       const sqlite3_destructor_type &__sqlite_destructor) {
+  const int __index = sqlite3_bind_parameter_index(_M_sqlite_stmt, __name);
+  int rc = sqlite3_bind_int64(_M_sqlite_stmt, __index, __value);
+  _M_check(rc);
+}
+
+template <>
+inline void stmt::bind(const char *__name, const double &__value,
+                       const sqlite3_destructor_type &__sqlite_destructor) {
+  const int __index = sqlite3_bind_parameter_index(_M_sqlite_stmt, __name);
+  int rc = sqlite3_bind_double(_M_sqlite_stmt, __index, __value);
+  _M_check(rc);
+}
+
+template <>
+inline void stmt::bind(const int &__index, const void *__value,
+                       const int &__size,
+                       const sqlite3_destructor_type &__sqlite_destructor) {
+  int rc = sqlite3_bind_blob(_M_sqlite_stmt, __index, __value, __size,
+                             __sqlite_destructor);
+  _M_check(rc);
+}
+
+template <>
+inline void stmt::bind(const char *__name, const void *__value,
+                       const int &__size,
+                       const sqlite3_destructor_type &__sqlite_destructor) {
+  const int __index = sqlite3_bind_parameter_index(_M_sqlite_stmt, __name);
+  int rc = sqlite3_bind_blob(_M_sqlite_stmt, __index, __value, __size,
+                             __sqlite_destructor);
+  _M_check(rc);
+}
+
+// {@ end bind
+
+// {@ get
+template <>
+inline const std::string stmt::get<std::string>(const int &__index) {
+  const char *__raw_ptr = reinterpret_cast<const char *>(
+      sqlite3_column_text(_M_sqlite_stmt, __index));
+  const int __size = sqlite3_column_bytes(_M_sqlite_stmt, __index);
+  return std::string(__raw_ptr, __size);
+}
+
+template <> inline const int stmt::get<int>(const int &__index) {
+  return sqlite3_column_int(_M_sqlite_stmt, __index);
+}
+
+template <> inline const long stmt::get<long>(const int &__index) {
+  return sqlite3_column_int64(_M_sqlite_stmt, __index);
+}
+
+template <> inline const double stmt::get<double>(const int &__index) {
+  return sqlite3_column_double(_M_sqlite_stmt, __index);
+}
+// {@ end get
+
+void util::check(const session &__s, const int &__rc) {
+  if (SQLITE_OK != __rc) {
+    throw sqlite_exception(__rc, __s.err_msg());
+  }
+}
 
 } // namespace sqlite_v3
 
--- test/sqlite/sqlite_test.cpp
+++ test/sqlite/sqlite_test.cpp
@@ -1,9 +1,41 @@
 #include <gtest/gtest.h>
+#include <varlib/log.hpp>
 #include <varlib/sqlite/sqlite.hpp>
 
 TEST(sqlite, session) {
   using session = varlib::sqlite_v3::session;
   session s("test.db");
-  ASSERT_EQ(true, s.is_initialied());
+  remove("test.db");
+}
+
+TEST(sqlite, stmt) {
+
+  using session = varlib::sqlite_v3::session;
+  using stmt = varlib::sqlite_v3::stmt;
+
+  session s{"test.db"};
+
+  int rc = s.exec("create table if not exists people(id TEXT primary key, name "
+                  "TEXT, age int);");
+  LOGI("rc = {}", rc);
+
+  rc = s.exec("insert into people values('a', 'a', 1);");
+  LOGI("rc = {}", rc);
+
+  rc = s.exec("insert into people values('b', 'a', 1);");
+  LOGI("rc = {}", rc);
+
+  rc = s.exec("update people set name = 'b' where id = 'b';");
+  LOGI("rc = {}", rc);
+
+  stmt st(s, "select * from people where id = ? and name = :name;");
+  st.bind(1, "b");
+  st.bind(":name", "b");
+  while ((rc = st.exec()) == varlib::sqlite_v3::stmt::ROW) {
+    std::string id = st.get<std::string>(0);
+    std::string name = st.get<std::string>(1);
+    int age = st.get<int>(2);
+    LOGI("id = {}, name = {}, age = {}", id, name, age);
+  }
   remove("test.db");
 }
